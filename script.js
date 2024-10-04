be5525a68b2433b60aa27e01b0c2034d67c0d5f0
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2024-10-04 07:39:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// const layout1 = document.getElementById('layout-1');
+// const sentinel = document.createElement('div');
+// sentinel.className = 'sentinel';
+// layout1.appendChild(sentinel);
+
+// const observer = new IntersectionObserver((entries) => {
+//   if (entries[0].isIntersecting) {
+//     // Trigger the animation when the sentinel comes into view
+//     layout1.classList.add('animate');
+//   }
+// }, {
+//   threshold: 0.5 // trigger when 50% of the sentinel is visible
+// });
+
+// observer.observe(sentinel);
